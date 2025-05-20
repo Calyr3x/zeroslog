03507c14a7721806d5b3c645b9dad4a03033b9e2
Aleksandr Ochkalenko <aleksandr.ochkalenko@iteco.com>
2025-05-20 10:53:22 +0300

    added test

--- go.mod
+++ go.mod
@@ -3,10 +3,18 @@ module github.com/Calyr3x/zeroslog
 go 1.24.3
 
 require (
-	github.com/bytedance/sonic v1.13.2 // indirect
+	github.com/bytedance/sonic v1.13.2
+	github.com/rs/zerolog v1.34.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
---
+++ test/bench_test.go
@@ -0,0 +1,173 @@
+package test
+
+import (
+	"errors"
+	"github.com/Calyr3x/zeroslog"
+	"io"
+	"log/slog"
+	"testing"
+	"time"
+
+	_ "github.com/rs/zerolog/log"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	fullMethod = "/conveyor.NotificationService/GetAmountUnreadNotifications"
+	dur        = 320 * time.Microsecond
+	version    = 21
+	someFloat  = 123.123
+	errTest    = errors.New("this is an error")
+)
+
+/*func BenchmarkZeroLog_Info(b *testing.B) {
+	w := zerolog.ConsoleWriter{
+		Out:        io.Discard,
+		TimeFormat: "2006-01-02 15:04:05.000 -07:00",
+		NoColor:    false,
+	}
+
+	zlog := zerolog.New(w)
+
+	benchLog(b, func() {
+		zlog.Info().
+			Str("method", fullMethod).
+			Dur("duration", dur).
+			Msg("gRPC call succeeded")
+	})
+}
+
+func BenchmarkZap_Info(b *testing.B) {
+	encCfg := zapcore.EncoderConfig{
+		TimeKey:     "ts",
+		MessageKey:  "msg",
+		LevelKey:    "level",
+		EncodeTime:  zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.000 -07:00"),
+		EncodeLevel: zapcore.CapitalLevelEncoder,
+	}
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encCfg),
+		zapcore.AddSync(io.Discard),
+		zap.InfoLevel,
+	)
+	zlogger := zap.New(core).Sugar()
+
+	benchLog(b, func() {
+		zlogger.Infow("gRPC call succeeded",
+			"method", fullMethod,
+			"duration", dur,
+		)
+	})
+}*/
+
+func BenchmarkSlog_Info(b *testing.B) {
+	slogLogger := slog.New(
+		slog.NewTextHandler(io.Discard, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}),
+	)
+
+	benchLog(b, func() {
+		slogLogger.Info("gRPC call succeeded",
+			"method", fullMethod,
+			"duration", dur,
+			"version", version,
+			"someFloat", someFloat,
+		)
+	})
+}
+
+func BenchmarkZeroSLog_Info(b *testing.B) {
+	logger := zeroslog.New(
+		zeroslog.WithTimeFormat("2006-01-02 15:04:05.000 -07:00"),
+		zeroslog.WithOutput(io.Discard),
+		zeroslog.WithColors(),
+	)
+
+	benchLog(b, func() {
+		logger.Info("gRPC call succeeded",
+			"method", fullMethod,
+			"duration", dur,
+			"version", version,
+			"someFloat", someFloat,
+		)
+	})
+}
+
+func BenchmarkLogrus_Info(b *testing.B) {
+	l := logrus.New()
+	l.SetOutput(io.Discard)
+	l.SetFormatter(&logrus.TextFormatter{
+		ForceColors:     true,
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05.000 -07:00",
+	})
+
+	benchLog(b, func() {
+		l.WithFields(logrus.Fields{
+			"method":    fullMethod,
+			"duration":  dur,
+			"version":   version,
+			"someFloat": someFloat,
+		}).Info("gRPC call succeeded")
+	})
+}
+
+func BenchmarkSlog_Error(b *testing.B) {
+	slogLogger := slog.New(
+		slog.NewTextHandler(io.Discard, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}),
+	)
+
+	benchLog(b, func() {
+		slogLogger.Error("gRPC call succeeded",
+			"method", fullMethod,
+			"duration", dur,
+			"err", errTest,
+		)
+	})
+}
+
+func BenchmarkZeroSLog_Error(b *testing.B) {
+	logger := zeroslog.New(
+		zeroslog.WithTimeFormat("2006-01-02 15:04:05.000 -07:00"),
+		zeroslog.WithOutput(io.Discard),
+		zeroslog.WithColors(),
+	)
+
+	benchLog(b, func() {
+		logger.Error("gRPC call succeeded",
+			"method", fullMethod,
+			"duration", dur,
+			"err", errTest,
+		)
+	})
+}
+
+func BenchmarkLogrus_Error(b *testing.B) {
+	l := logrus.New()
+	l.SetOutput(io.Discard)
+	l.SetFormatter(&logrus.TextFormatter{
+		ForceColors:     true,
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05.000 -07:00",
+	})
+
+	benchLog(b, func() {
+		l.WithFields(logrus.Fields{
+			"method":   fullMethod,
+			"duration": dur,
+		}).Error("err", errTest)
+	})
+}
+
+func benchLog(b *testing.B, fn func()) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fn()
+	}
+}
